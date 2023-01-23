e51c6bc652a7227472cd0f86dbf2e849cfb3794d
Mary Ouma <maryawish54@gmail.com>
2023-01-23 05:41:14 -0800

    quick sort

--- sort.h
+++ sort.h
@@ -33,6 +33,8 @@ void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
 
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
